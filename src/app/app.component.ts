6c3d9183bec466b791f3ada7eba295e740f2473d
gbrzied <gbrzied@gmail.com>
2024-09-26 08:52:53 +0100

    calendar added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,25 @@
 import { Component } from '@angular/core';
+import { CalendarEvent } from 'angular-calendar';
+import { addDays, addHours, startOfDay } from 'date-fns';
+
+
+export const colors: any = {
+  red: {
+    primary: '#ad2121',
+    secondary: '#FAE3E3',
+  },
+  blue: {
+    primary: '#1e90ff',
+    secondary: '#D1E8FF',
+  },
+  yellow: {
+    primary: '#e3bc08',
+    secondary: '#FDF1BA',
+  },
+};
+
+
+
 @Component(
   { selector: 'app-root', 
   template: `
@@ -7,11 +28,42 @@ import { Component } from '@angular/core';
 
   <p> Length: {{title.length}} </p > 
   
-  <p>Reversed: {{ getReversed(title) }} </p> `,
+  <p>Reversed: {{ getReversed(title) }} </p> 
+  <mwl-calendar-week-view
+  [viewDate]="viewDate"
+  [events]="events"
+>
+</mwl-calendar-week-view>
+  `,
    styles: [] })
   
 
   export class AppComponent {
+
+
+
+
+    viewDate: Date = new Date();
+
+    events: CalendarEvent [] = [
+      {
+        start: startOfDay(new Date()),
+        title: 'An event',
+        color: colors.yellow,
+      },
+      {
+        start: addHours(startOfDay(new Date()), 2),
+        end: new Date(),
+        title: 'Another event',
+        color: colors.blue,
+      },
+      {
+        start: addDays(addHours(startOfDay(new Date()), 2), 2),
+        end: addDays(new Date(), 2),
+        title: 'And another',
+        color: colors.red,
+      },
+    ];
   title = 'welcome to app!';
   getReversed(str: string) {
     let reversed = ''; 
